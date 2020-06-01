b681f9063b571f819788ad66b487651c46e08673
Sergey Platonov <platserg94@yahoo.com>
2020-06-01 12:35:01 +0200

    first

---
+++ __main__.py
@@ -0,0 +1,20 @@
+
+from math import sin, cos
+import numpy as np
+
+def tfd():
+    pass
+
+def tfd2():
+    pass
+
+def tfdi():
+    pass
+
+def tfdi2():
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
